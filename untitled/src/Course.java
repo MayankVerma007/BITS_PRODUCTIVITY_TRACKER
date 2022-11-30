654ef640a51100153da811f0c3a0890708751948
FahdSeth28 <87655858+FahdSeth28@users.noreply.github.com>
2022-11-30 18:07:54 +0530

    Update Course.java

--- untitled/src/Course.java
+++ untitled/src/Course.java
@@ -1,20 +1,42 @@
+import java.util.Scanner;
+import java.util.SortedMap;
+
 public class Course {
     private int courseUnits;
-    private String[] lectureHours;  //Each entry in the String array is of the form "Day-Hour"
+
+    private String courseTitle;
+    private int[] lectureHours;  //Each entry in the String array is of the form "Day-Hour"
     private int tutorialSection;
     private int lectureSection;
-    private String tutorialHour; //Mentioned as "Day-Hour"
-    private int numberOfEvals;
+    private int tutorialHour; //Mentioned as "Day-Hour"
+
+    private String lectureInstructor;
+
+    private String tutInstructor;
+
+    private int labSection;
+    private int labHour;
 
 
 
-    public Course(int courseUnits, String[] lectureHours, int tutorialSection, int lectureSection, String tutorialHour, int numberOfEvals) {
-        this.courseUnits = courseUnits;
-        this.lectureHours = lectureHours;
-        this.tutorialSection = tutorialSection;
-        this.lectureSection = lectureSection;
-        this.tutorialHour = tutorialHour;
-        this.numberOfEvals = numberOfEvals;
+//    public Course(int courseUnits, int[] lectureHours, int tutorialSection, int lectureSection, int tutorialHour, int labSection, int labHour) {
+//        this.courseUnits = courseUnits;
+//        this.lectureHours = lectureHours;
+//        this.tutorialSection = tutorialSection;
+//        this.lectureSection = lectureSection;
+//        this.tutorialHour = tutorialHour;
+//        this.labHour = labHour;
+//        this.labSection = labSection;
+//    }
+
+
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
     }
 
     public int getCourseUnits() {
@@ -25,11 +47,11 @@ public class Course {
         this.courseUnits = courseUnits;
     }
 
-    public String[] getLectureHours() {
+    public int[] getLectureHours() {
         return lectureHours;
     }
 
-    public void setLectureHours(String[] lectureHours) {
+    public void setLectureHours(int[] lectureHours) {
         this.lectureHours = lectureHours;
     }
 
@@ -49,19 +71,109 @@ public class Course {
         this.lectureSection = lectureSection;
     }
 
-    public String getTutorialHour() {
+    public int getTutorialHour() {
         return tutorialHour;
     }
 
-    public void setTutorialHour(String tutorialHour) {
+    public void setTutorialHour(int tutorialHour) {
         this.tutorialHour = tutorialHour;
     }
 
-    public int getNumberOfEvals() {
-        return numberOfEvals;
+    public String getLectureInstructor() {
+        return lectureInstructor;
     }
 
-    public void setNumberOfEvals(int numberOfEvals) {
-        this.numberOfEvals = numberOfEvals;
+    public void setLectureInstructor(String lectureInstructor) {
+        this.lectureInstructor = lectureInstructor;
+    }
+
+    public String getTutInstructor() {
+        return tutInstructor;
+    }
+
+    public void setTutInstructor(String tutInstructor) {
+        this.tutInstructor = tutInstructor;
+    }
+
+    public int getLabSection() {
+        return labSection;
+    }
+
+    public void setLabSection(int labSection) {
+        this.labSection = labSection;
+    }
+
+    public int getLabHour() {
+        return labHour;
+    }
+
+    public void setLabHour(int labHour) {
+        this.labHour = labHour;
+    }
+
+    public Course toAddNewCourse()
+    {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter course details-");
+        //Course instructor
+        System.out.print("Enter lecture Instructor");
+        this.lectureInstructor = sc.nextLine();
+        //tut instructor
+        System.out.print("Enter tut instructor");
+        this.tutInstructor = sc.nextLine();
+        //course name
+        System.out.print("Course Name-");
+        this.courseTitle = sc.nextLine();
+        //course units
+        System.out.print("Course Units-");
+        this.courseUnits = sc.nextInt();
+        //lecture section
+        System.out.print("Lecture section-");
+        this.lectureSection= sc.nextInt();
+        sc.nextLine();
+        //lecture hour
+        System.out.print("Lecture Hours-");
+        String hourList = sc.nextLine();
+        String[] hourLst = hourList.split(" ");
+        for(int i=0;i<hourLst.length;i++)
+        {
+            int value = Integer.parseInt(hourLst[i]);
+        }
+
+        System.out.println("Tutorial Exists? Yes/No");
+
+        String ans = sc.nextLine();
+        switch(ans)
+        {
+            case "Yes":
+                System.out.println("Enter tut section and hour.");
+                String details = sc.nextLine();
+                //tutorial section and hour
+                this.tutorialSection= Integer.parseInt(details.split(" ",0)[0]);
+                this.tutorialHour = Integer.parseInt(details.split(" ",0)[1]);
+                break;
+            default:
+                break;
+        }
+        System.out.println("Lab Exists? Yes/No");
+        ans = sc.nextLine();
+        switch(ans)
+        {
+            case "Yes":
+                System.out.println("Enter lab section.");
+                String details = sc.nextLine();
+                //lab section and hour
+                this.labSection = Integer.parseInt(details.split(" ",0)[0]);
+                this.labHour = Integer.parseInt(details.split(" ",0)[1]);
+                break;
+            default:
+                break;
+        }
+        System.out.println("Course Sucessfully Created");
+
+        return this;
+
     }
 }
+
